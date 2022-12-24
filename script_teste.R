bf5d7ee53566d12a7c107c5511cdcc3eaf024eb9
Lucas Coraça Germano <lucascgermano@gmail.com>
2022-12-24 11:19:44 -0300

    correcao mas as idades estao com muito NA

--- script_teste.R
+++ script_teste.R
@@ -99,3 +99,54 @@ format(seq.Date(from = as.Date("2021-01-01"),
 
 format(Sys.Date(), format="%b/%y")
 
+
+# separador ---------------------------------------------------------------
+
+dd <- fread(here('dados','todos_anos.csv'))
+casos <- dd %>%
+  group_by(data) %>%
+  summarise(casos = sum(casos, na.rm=T)) %>% 
+  mutate(data = as.Date(data),
+         mmovel7 = round(rollmean(casos, k = 7, fill = NA, align = 'left'),0))
+
+#Doses
+doses.data <- tabyl(ddvac, data, dose, show_na = F) %>%
+  mutate(data = as.Date(data)) %>% 
+  setNames(c('data','d1','d2','ref1','ref2','ref3','ref4'))
+
+#Juntando casos, doses e limpando NA
+casos.doses <- left_join(x = casos, y = doses.data, by="data")
+casos.doses[is.na(casos.doses)] <- 0
+
+#Gerando arquivo para o gráfico
+graf <- cbind(casos.doses,
+              round(cumsum(x = casos.doses$d1)/766509*100, 1),
+              round(cumsum(x = casos.doses$d2)/766509*100, 1),
+              round(cumsum(x = casos.doses$ref1)/766509*100, 1),
+              round(cumsum(x = casos.doses$ref2)/766509*100, 1),
+              round(cumsum(x = casos.doses$ref3)/766509*100, 1),
+              round(cumsum(x = casos.doses$ref4)/766509*100, 1)) %>%
+  setNames(c("data","casos","mmovel","d1","d2","ref1","ref2","ref3","ref4",
+             'cob.d1','cob.d2','cob.ref1','cob.ref2','cob.ref3','cob.ref4'))
+
+#Gráfico
+ggplot(graf[year(graf$data)!='2020',])+
+  geom_col(aes(x = data, y = mmovel), alpha = .25, width = 1, fill='#00598C')+  
+  geom_line(aes(x = data, y = cob.d1*20, col='Cob.d1'))+
+  geom_line(aes(x = data, y = cob.d2*20, col='Cob.d2'))+
+  geom_line(aes(x = data, y = cob.ref1*20, col='Cob.ref1'))+
+  geom_line(aes(x = data, y = cob.ref2*20, col='Cob.ref2'))+
+  geom_line(aes(x = data, y = cob.ref3*20, col='Cob.ref3'))+
+  scale_colour_manual(values = RColorBrewer::brewer.pal(n = 6, name = 'Paired'), 
+                      labels = c("D1",'D2','Reforço 1', 'Reforço 2', 'Reforço 3'))+
+  # scale_fill_manual(breaks = c('Cobertura de D1','Cobertura de D2',"Dose adicional"))+
+  scale_x_date(date_breaks = "2 month", date_labels = "%b/%y")+
+  scale_y_continuous(sec.axis = sec_axis(~./20, name = "Cobertura (%)", breaks = seq(0,120,5)))+
+  labs(x=NULL, y='Casos', colour=NULL, title = "Casos e coberturas vacinais por dia.")+
+  theme_minimal()+
+  theme(plot.title = element_text(hjust = .5, face = "bold", colour = alpha('black', .7)),
+        legend.position = 'top', axis.text.x = element_text(angle = 30),
+        legend.key = element_rect(fill = 'transparent', 
+                                  colour = 'transparent'),
+        legend.box.background = element_rect(fill = 'transparent', 
+                                             colour = 'transparent'))

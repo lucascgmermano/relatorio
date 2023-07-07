a61ff91cf7e505576a1a05bdecc0f75473bf5b3e
Lucas Coraça Germano <lucascgermano@gmail.com>
2023-07-07 00:26:10 -0300

    colaboracao

--- script_teste.R
+++ script_teste.R
@@ -1,1 +1,9 @@
 .
+install.packages("usethis")
+usethis::use_git()
+usethis::use_git_config(
+  user.name = "Lucas Coraça Germano",
+  user.email = "lucascgermano@gmail.com"
+)
+
+credentials::ssh_setup_github()

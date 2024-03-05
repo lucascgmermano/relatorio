4980600f3c4014e7de0b36c5084cb4c3f5862ffe
Lucas Coraça Germano <lucascgermano@gmail.com>
2024-03-04 21:27:14 -0300

    commit 3

--- renderizar_tudo.R
+++ renderizar_tudo.R
@@ -28,7 +28,7 @@ quadro[quadro$mesL %in% c("jan","fev") & quadro$ano == 2020,'casos'] <- 0
 ggplot(quadro, aes(x = mesL, y = ano, fill = casos)) +
   geom_tile() + # Usar geom_tile para criar o heatmap
   geom_text(aes(label= casos), color="red3", check_overlap = TRUE) +
-  scale_fill_gradient(low = "#D1E5F0", high = "blue4") + # Definir cores do gradiente
+  scale_fill_gradient(low = "#D1E5F0", high = "blue2") + # Definir cores do gradiente
   theme_minimal() + # Tema minimalista
   labs(fill = "Número de Casos", x = NULL, y = NULL, title = "Heatmap de Casos de COVID-19") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) # Ajustar texto do eixo x
@@ -53,3 +53,6 @@ acumulado[,1] <- c("Casos", "Óbitos")
 Total <- rowSums(acumulado[,2:6])
 
 cbind(acumulado, total)
+
+
+heat.colors(5)
